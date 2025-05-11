e4463adb2323d8079baab05969f743c08e2fdd57
Vincent Hiribarren <vhiribarren@users.noreply.github.com>
2025-05-11 16:59:17 +0200

    update bloom effect with invert

--- study-bloom-effect-2d/main.glsl
+++ study-bloom-effect-2d/main.glsl
@@ -2,6 +2,8 @@
 // https://creativecommons.org/licenses/by-nc-sa/4.0/
 // Author: Vincent Hiribarren
 
+const float SEGMENT_HEIGHT = 0.01;
+
 // https://iquilezles.org/articles/distfunctions2d/
 float sdSegment( in vec2 p, in vec2 a, in vec2 b ) {
     vec2 pa = p-a, ba = b-a;
@@ -16,12 +18,13 @@ void mainImage( out vec4 fragColor, in vec2 fragCoord ) {
 
     // Simple segment
     float d1 = sdSegment(uv, vec2(0.25, 0.2), vec2(0.75, 0.2));
-    d1 = 1.0 - step(0.01, d1);
+    d1 = 1.0 - step(SEGMENT_HEIGHT, d1);
     col += vec3(d1);
 
     // Bloom using inverse function
     float d2 = sdSegment(uv, vec2(0.25, 0.4), vec2(0.75, 0.4));
-    d2 = 0.01 / d2;
+    float bloom_strength = 0.01;
+    d2 = bloom_strength / (d2 - (SEGMENT_HEIGHT - bloom_strength));
     col += vec3(d2);
 
     // Bloom with smoothstep
@@ -31,7 +34,7 @@ void mainImage( out vec4 fragColor, in vec2 fragCoord ) {
 
     // Bloom with an exponential function
     float d4 = sdSegment(uv, vec2(0.25, 0.8), vec2(0.75, 0.8));
-    d4 = exp(-100.0 * (d4 - 0.01));
+    d4 = exp(-100.0 * (d4 - SEGMENT_HEIGHT));
     col += vec3(d4);
 
     fragColor = vec4(col, 1.0);
