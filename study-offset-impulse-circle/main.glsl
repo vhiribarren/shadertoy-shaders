6cd8ef3011bdb4d9582c9c3c41441daaef327890
Vincent Hiribarren <vhiribarren@users.noreply.github.com>
2025-05-30 22:06:07 +0200

    add study-offset-impulse-circle

---
+++ study-offset-impulse-circle/main.glsl
@@ -0,0 +1,33 @@
+// SPDX-License-Identifier: CC-BY-NC-SA-4.0
+// https://creativecommons.org/licenses/by-nc-sa/4.0/
+// Author: Vincent Hiribarren
+
+// Radius of the base circle drawn with a gradient tone
+const float RADIUS = 0.01;
+// Number of waves between coordinates [0;1]
+const float FREQ = 1.;
+// Strenght of offset
+const float AMP = .01;
+// Propagation speed of waves
+const float SPEED = .1;
+// Reset animation every period
+const float PERIOD = 12.;
+
+const float PI = 3.14;
+
+void mainImage( out vec4 fragColor, in vec2 fragCoord )
+{
+    // [-1;1] coordinates and ratio fix
+    vec2 uv = 2.*(.5-fragCoord.xy/iResolution.xy)*vec2(iResolution.x/iResolution.y,1.);
+
+    float periodicTime = mod(iTime, PERIOD);
+
+    float radius = length(uv  - normalize(uv)*periodicTime*SPEED);
+    float offset = AMP*sin(2.*PI*FREQ*radius)/(0.01+pow(radius, 2.));
+
+    vec3 signed_dist = vec3(length(uv + offset*normalize(uv)) - RADIUS);
+    vec3 col = 1.0 - signed_dist;
+
+    // Output to screen
+    fragColor = vec4(col,1.0);
+}
